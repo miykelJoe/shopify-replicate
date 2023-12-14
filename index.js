811597b51fb054722464e3b802ea8f24c64b8bdc
nekeoke <ukwuchinaekeoke@gmail.com>
2023-12-14 17:22:32 +0000

    making the list pop out when clicked

--- index.js
+++ index.js
@@ -4,21 +4,46 @@ const arrowDown = document.querySelector('.arrow-down');
 const arrowUp = document.querySelector('.arrow-up');
 const collapsibleTrigger = document.querySelector('.collapsible-trigger');
 const options = document.querySelector('.options');
+const steps = document.querySelectorAll('.steps');
+// const paragraph = document.querySelectorAll('.para-btn');
 
-let tog  = true
-cancel.onclick = function () {
-    if (tog){
-        tog = false
-        aside.style.display = 'none'
-    }
+
+steps[0].classList.add('active');
+
+function toggleCard(clickedCard) {
+
+    // If the clicked card is already active, return
+    if (clickedCard.classList.contains('active')) return
+
+
+    // Loop through all cards to deactivate other active cards
+    steps.forEach((card) => {
+        if (card !== clickedCard && card.classList.contains('active')) {
+            card.classList.remove('active');
+        }
+    });
+
+     // Toggle the 'active' class for the clicked card
+  clickedCard.classList.add('active');
 }
 
-collapsibleTrigger.onclick = function (){
-    if (arrowDown.style.display == 'block'){
+steps.forEach((card) => {
+    card.addEventListener('click', function() {
+        toggleCard(card);
+    }) 
+})
+
+
+cancel.onclick = function () {
+    aside.style.display = 'none'
+}
+
+collapsibleTrigger.onclick = function () {
+    if (arrowDown.style.display == 'block') {
         arrowDown.style.display = 'none';
         arrowUp.style.display = 'block';
         options.style.display = 'none';
-    }else if (arrowDown.style.display == 'none'){
+    } else if (arrowDown.style.display == 'none') {
         arrowDown.style.display = 'block';
         arrowUp.style.display = 'none';
         options.style.display = 'block';
