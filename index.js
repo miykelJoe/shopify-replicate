2b349467f0b03ca255fad52dd8f02c5c0d423c0b
nekeoke <ukwuchinaekeoke@gmail.com>
2023-12-14 22:48:13 +0000

    adding responsiveness

--- index.js
+++ index.js
@@ -5,40 +5,71 @@ const arrowUp = document.querySelector('.arrow-up');
 const collapsibleTrigger = document.querySelector('.collapsible-trigger');
 const options = document.querySelector('.options');
 const steps = document.querySelectorAll('.steps');
-// const paragraph = document.querySelectorAll('.para-btn');
 
 
-steps[0].classList.add('active');
+// steps[0].classList.add('active');
 
-function toggleCard(clickedCard) {
+// function toggleCard(clickedCard) {
 
-    // If the clicked card is already active, return
-    if (clickedCard.classList.contains('active')) return
+//     // If the clicked card is already active, return
+//     if (clickedCard.classList.contains('active')) return
 
 
-    // Loop through all cards to deactivate other active cards
-    steps.forEach((card) => {
-        if (card !== clickedCard && card.classList.contains('active')) {
-            card.classList.remove('active');
-        }
-    });
+//     // Loop through all cards to deactivate other active cards
+//     steps.forEach((card) => {
+//         if (card !== clickedCard && card.classList.contains('active')) {
+//             card.classList.remove('active');
+//         }
+//     });
 
-     // Toggle the 'active' class for the clicked card
-  clickedCard.classList.add('active');
-}
+//      // Toggle the 'active' class for the clicked card
+//   clickedCard.classList.add('active');
+// }
 
-steps.forEach((card) => {
-    card.addEventListener('click', function() {
-        toggleCard(card);
-    }) 
-})
+// steps.forEach((card) => {
+//     card.addEventListener('click', function() {
+//         toggleCard(card);
+//     }) 
+// })
+
+
+steps.forEach( function(card){
+    
+    card.onclick = function(event){
+
+        // this code checks if the card that was clicked on have the active class
+        if(card.classList.contains("active")) return 
+
+        // this code removes the active class from all steps element
+        steps.forEach( card => card.classList.remove("active"))
+
+        // this code adds the active class to the card (step) click on
+        card.classList.add("active")
+    }
+
+} )
+
+
 
 
 cancel.onclick = function () {
     aside.style.display = 'none'
 }
 
+
+const collapsContainer = document.querySelector("main")
+
+
+
 collapsibleTrigger.onclick = function () {
+    // collapsContainer.classList.toggle("hide")
+
+    // if(collapsContainer.classList.contains("hide")){
+    //     collapsContainer.classList.remove("hide")
+    // }else{
+    //     collapsContainer.classList.add("hide")
+    // }
+
     if (arrowDown.style.display == 'block') {
         arrowDown.style.display = 'none';
         arrowUp.style.display = 'block';
